ca3eb0a779639741983678d59fdef1943fd1a707
DESKTOP-K72OSB8\beomukim <gld55602@gmal.com>
2021-08-25 15:30:05 +0900

    함수

--- main.js
+++ main.js
@@ -2,49 +2,60 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
  
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-
     if(pathname === '/'){
+      fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${title}`, 'utf8', function(err, description){
-            
-             if(title === undefined){
-               
-                title = 'Welcome';
-                description = 'Hello, Node.js';
-                 
-             } 
-             fs.readdir('./data', function(error, filelist){
-              var list = '<ul>';
-              for (var i = 0; i < filelist.length; i++) {
-                list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-              }
-              list = list+'</ul>';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+            if(title === undefined){
+              title = 'Welcome';
+              description = 'Hello, Node.js';
+            }
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
+          });
         });
-      })
+      
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
+    console.log(pathname);
+    console.log(title);
+ 
+ 
+ 
 });
 app.listen(3000);
